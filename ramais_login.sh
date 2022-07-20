f55ebce1ccfbd0bcd5db9b661353466697ee5b74
josimarrocha0197 <45229686+josimarrocha0197@users.noreply.github.com>
2022-07-20 13:04:08 -0300

    Add files via upload

--- ramais_login.sh
+++ ramais_login.sh
@@ -12,31 +12,41 @@ function lastX {
                 echo -e "Por favor, informe um parâmetro válido (90|60|30) \n"
                 exit
         fi
-	separator
-	echo -e "Valor referência: '$1' dias"
-	separator
-	parameter=-${1}
+        separator
+        echo -e "Valor referência: '$1' dias"
+        separator
+        parameter=-${1}
 
-	mapfile -t extensionAll < <(mysql ${sqlOptions} --batch -N futurofone -e 'SELECT 'numero_ramal' FROM 'pbx_ramal'')
-	mapfile -t extensionLogin < <(mysql ${sqlOptions} --batch -N futurofone -e 'SELECT DISTINCT 'ramal_cdrqueuelogin' AS ramal FROM 'pbx_cdrqueuelogin' WHERE 'time_cdrqueuelogin' BETWEEN DATE_ADD( CURRENT_DATE( ) , INTERVAL '${parameter}' DAY ) AND DATE_ADD( CURRENT_DATE( ) , INTERVAL +1 DAY )' | grep -v NULL)
+        mapfile -t extensionAll < <(mysql ${sqlOptions} --batch -N futurofone -e 'SELECT 'numero_ramal' FROM 'pbx_ramal'')
+        mapfile -t extensionAllPa < <(mysql ${sqlOptions} --batch -N futurofone -e 'SELECT 'numero_ramal' FROM 'pbx_ramal' WHERE 'tipo_ramal'="2" OR 'tipo_ramal'="5"')
+        mapfile -t extensionLogin < <(mysql ${sqlOptions} --batch -N futurofone -e 'SELECT DISTINCT 'ramal_cdrqueuelogin' AS ramal FROM 'pbx_cdrqueuelogin' WHERE 'time_cdrqueuelogin' BETWEEN DATE_ADD( CURRENT_DATE( ) , INTERVAL '${parameter}' DAY ) AND DATE_ADD( CURRENT_DATE( ) , INTERVAL +1 DAY )' | grep -v NULL)
 
-	echo -e "Ramais do cliente:"
-	echo -e "${extensionAll[@]}" | tr ' ' ','
-	echo -e "Quantidade: ${#extensionAll[@]}"
-	separator
-	echo -e "Ramais que logaram no painel nos ultimos '$1' dias:"
-	echo -e "**Pode conter ramais INATIVOS"
-	echo -e "${extensionLogin[@]}" | tr ' ' ','
-	echo -e "Quantidade: ${#extensionLogin[@]}"
-	
-	mapfile -t commExtensionResult < \
+        echo -e "Ramais do cliente:"
+        echo -e "${extensionAll[@]}" | tr ' ' ','
+        echo -e "Quantidade: ${#extensionAll[@]}"
+        separator
+        echo -e "Ramais que logaram no painel nos ultimos '$1' dias:"
+        echo -e "**Pode conter ramais INATIVOS"
+        echo -e "${extensionLogin[@]}" | tr ' ' ','
+        echo -e "Quantidade: ${#extensionLogin[@]}"
+
+        mapfile -t commExtensionResult < \
                 <(comm -23 \
                         <(IFS=$'\n'; echo "${extensionAll[*]}" | sort) \
                         <(IFS=$'\n'; echo "${extensionLogin[*]}" | sort) \
-                )		
-	separator
-	echo -e "Ramais que não logaram no painel nos ultimos '$1' dias:"
-	echo -e "${commExtensionResult[@]}" | tr ' ' ','
-	}
+                )
+        separator
+        echo -e "Ramais que não logaram no painel nos ultimos '$1' dias:"
+        echo -e "${commExtensionResult[@]}" | tr ' ' ','
+
+        mapfile -t commExtensionResultPa < \
+                <(comm -23 \
+                        <(IFS=$'\n'; echo "${extensionAllPa[*]}" | sort) \
+                        <(IFS=$'\n'; echo "${extensionLogin[*]}" | sort) \
+                )
+        separator
+        echo -e "Ramais do tipo PA que não logaram no painel nos ultimos '$1' dias:"
+        echo -e "${commExtensionResultPa[@]}" | tr ' ' ','
+        }
 lastX $1
 separator
